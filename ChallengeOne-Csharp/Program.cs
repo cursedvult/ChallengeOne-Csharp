fdd848d1db67d6ab214c2b5f8cc7b86bcdc1e062
Gustavo Miguel De Barros Carranço <GutMig1112@outlook.com>
2023-03-09 05:49:30 -0300

    Writeline to Write

--- ChallengeOne-Csharp/Program.cs
+++ ChallengeOne-Csharp/Program.cs
@@ -7,7 +7,7 @@ namespace ChallengeOne_Csharp
         static void Main(string[] args)
         {
             string myString;
-            Console.WriteLine("Please enter your name and press enter:");
+            Console.Write("Please enter your name and press enter:");
             myString = Console.ReadLine();
             Console.WriteLine(myString.ToUpper());
             Console.WriteLine(myString.ToLower());
