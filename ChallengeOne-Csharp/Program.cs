fa102e39a9536dec63e5583c93e43dfce2736866
Gustavo Miguel De Barros Carranço <GutMig1112@outlook.com>
2023-03-09 05:45:33 -0300

    Add project files.

---
+++ ChallengeOne-Csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChallengeOne_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString;
+            Console.WriteLine("Please enter your name and press enter:");
+            myString = Console.ReadLine();
+            Console.WriteLine(myString.ToUpper());
+            Console.WriteLine(myString.ToLower());
+            Console.WriteLine(myString.Trim());
+            Console.WriteLine(myString.Substring(1));
+            Console.ReadKey();
+        }
+    }
+}
